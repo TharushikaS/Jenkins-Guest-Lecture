fb18b0792e6f1f93e8b853817f9cd03e7e277fcb
R.L.D.T.H. Surasinghe <146325963+TharushikaS@users.noreply.github.com>
2025-01-18 15:10:45 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,1 +1,13 @@
-
+pipeline {
+    agent any
+    stages {
+        stage('Create Directory') {
+            steps {
+                script {
+                    def dirPath = "/path/to/your/directory"
+                    sh "mkdir -p ${dirPath}"
+                }
+            }
+        }
+    }
+}
