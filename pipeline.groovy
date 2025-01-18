16a175ba03131649e008c4858f3330c4f95e155d
R.L.D.T.H. Surasinghe <146325963+TharushikaS@users.noreply.github.com>
2025-01-18 15:30:14 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,13 +3,11 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                // Clone the GitHub repository
-                git 'https://github.com/TharushikaS/Jenkins-Guest-Lecture.git'
+                git branch: 'main', url: 'https://github.com/TharushikaS/Jenkins-Guest-Lecture.git'
             }
         }
         stage('Run Python Script') {
             steps {
-                // Run the Python script
                 sh 'python3 hello_world.py'
             }
         }
